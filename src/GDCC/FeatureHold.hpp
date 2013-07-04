0e86dc286ea13bbaaadb9b084bc14df5da4a5fe1
DavidPH <DavidPH55@verizon.net>
2013-07-04 09:28:14 -0400

    Moved MageDefs::FeatureHold into GDCC.

---
+++ src/GDCC/FeatureHold.hpp
@@ -0,0 +1,40 @@
+//-----------------------------------------------------------------------------
+//
+// Copyright (C) 2013 David Hill
+//
+// See COPYING for license information.
+//
+//-----------------------------------------------------------------------------
+//
+// Template for holding off an IStream feature.
+//
+//-----------------------------------------------------------------------------
+
+#ifndef GDCC__FeatureHold_H__
+#define GDCC__FeatureHold_H__
+
+
+//----------------------------------------------------------------------------|
+// Types                                                                      |
+//
+
+namespace GDCC
+{
+   //
+   // FeatureHold
+   //
+   template<typename T, void(T::*D)(), void(T::*E)()> class FeatureHold
+   {
+   public:
+      FeatureHold(FeatureHold const &) = delete;
+      FeatureHold(FeatureHold &&hold) : str{hold.str} {hold.str = nullptr;}
+      explicit FeatureHold(T &str_) : str{&str_} {(str->*D)();}
+      ~FeatureHold() {if(str) (str->*E)();}
+
+   private:
+      T *str;
+   };
+}
+
+#endif//GDCC__FeatureHold_H__
+
--- src/MageDefs/IStream.hpp
+++ src/MageDefs/IStream.hpp
@@ -14,6 +14,7 @@
 #define MageDefs__IStream_H__
 
 #include "GDCC/CommentBuf.hpp"
+#include "GDCC/FeatureHold.hpp"
 #include "GDCC/OriginBuf.hpp"
 
 #include <istream>
@@ -31,21 +32,6 @@ namespace GDCC
 namespace MageDefs
 {
    //
-   // FeatureHold
-   //
-   template<typename T, void(T::*D)(), void(T::*E)()> class FeatureHold
-   {
-   public:
-      FeatureHold(FeatureHold const &) = delete;
-      FeatureHold(FeatureHold &&hold) : str{hold.str} {hold.str = nullptr;}
-      explicit FeatureHold(T &str_) : str{&str_} {(str->*D)();}
-      ~FeatureHold() {if(str) (str->*E)();}
-
-   private:
-      T *str;
-   };
-
-   //
    // IStream
    //
    class IStream : public std::istream
@@ -60,8 +46,8 @@ namespace MageDefs
 
       GDCC::Origin getOrigin() const {return obuf.getOrigin();}
 
-      FeatureHold<IStream, &IStream::disableComments, &IStream::enableComments> holdComments()
-         {return FeatureHold<IStream, &IStream::disableComments, &IStream::enableComments>(*this);}
+      GDCC::FeatureHold<IStream, &IStream::disableComments, &IStream::enableComments> holdComments()
+         {return GDCC::FeatureHold<IStream, &IStream::disableComments, &IStream::enableComments>(*this);}
 
    protected:
       typedef GDCC::OriginBuf<>               OBuf;
