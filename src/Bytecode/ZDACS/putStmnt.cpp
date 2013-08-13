c3c1f2627740d0c5b411a3f7695a6b9a90a5acf3
DavidPH <DavidPH55@verizon.net>
2013-08-13 11:13:41 -0400

    Migrated Bytecode/ZDACS IR::Statement handling code to its own files.

--- src/Bytecode/ZDACS/putStmnt.cpp
+++ src/Bytecode/ZDACS/putStmnt.cpp
@@ -12,7 +12,8 @@
 
 #include "Info.hpp"
 
-#include "GDCC/IR/Statement.hpp"
+#include "GDCC/IR/Function.hpp"
+#include "GDCC/IR/Glyph.hpp"
 
 #include <iostream>
 
@@ -25,6 +26,143 @@ namespace Bytecode
 {
    namespace ZDACS
    {
+      //
+      // Info::putStmnt
+      //
+      void Info::putStmnt(std::ostream &out, GDCC::IR::Statement const &stmnt)
+      {
+         switch(stmnt.code)
+         {
+         case GDCC::IR::Code::Nop:
+            putWord(out, 0);
+            break;
+
+         case GDCC::IR::Code::AddI_W:
+         case GDCC::IR::Code::AddU_W:
+            putWord(out, 14); // add
+            break;
+
+         case GDCC::IR::Code::AndU_W:
+            putWord(out, 72); // andbitwise
+            break;
+
+         case GDCC::IR::Code::Call:
+            putStmnt_Call(out, stmnt);
+            break;
+
+         case GDCC::IR::Code::Casm:
+            for(auto const &arg : stmnt.args)
+               putExpWord(out, arg.aLit.value);
+            break;
+
+         case GDCC::IR::Code::CmpI_EQ_W:
+         case GDCC::IR::Code::CmpU_EQ_W:
+            putWord(out, 19); // eq
+            break;
+
+         case GDCC::IR::Code::CmpI_GE_W:
+            putWord(out, 24); // ge
+            break;
+
+         case GDCC::IR::Code::CmpI_GT_W:
+            putWord(out, 22); // gt
+            break;
+
+         case GDCC::IR::Code::CmpI_LE_W:
+            putWord(out, 23); // le
+            break;
+
+         case GDCC::IR::Code::CmpI_LT_W:
+            putWord(out, 21); // lt
+            break;
+
+         case GDCC::IR::Code::CmpI_NE_W:
+         case GDCC::IR::Code::CmpU_NE_W:
+            putWord(out, 20); // ne
+            break;
+
+         case GDCC::IR::Code::Cnat:
+            putWord(out, 351); // callfunc
+            putExpWord(out, stmnt.args[0].aLit.value);
+            putWord(out, stmnt.args.size() - 2);
+            break;
+
+         case GDCC::IR::Code::Cspe:
+            putStmnt_Cspe(out, stmnt);
+            break;
+
+         case GDCC::IR::Code::DivI_W:
+            putWord(out, 17); // divide
+            break;
+
+         case GDCC::IR::Code::DivX_W:
+            putWord(out, 137); // fixeddiv
+            break;
+
+         case GDCC::IR::Code::InvU_W:
+            putWord(out, 330); // negatebinary
+            break;
+
+         case GDCC::IR::Code::ModI_W:
+            putWord(out, 18); // modulus
+            break;
+
+         case GDCC::IR::Code::Move_W:
+            putStmnt_Move_W(out, stmnt);
+            break;
+
+         case GDCC::IR::Code::MulI_W:
+            putWord(out, 16); // multiply
+            break;
+
+         case GDCC::IR::Code::MulX_W:
+            putWord(out, 136); // fixedmul
+            break;
+
+         case GDCC::IR::Code::NegI_W:
+            putWord(out, 78); // unaryminus
+            break;
+
+         case GDCC::IR::Code::NotU_W:
+            putWord(out, 75); // negatelogical
+            break;
+
+         case GDCC::IR::Code::OrIU_W:
+            putWord(out, 73); // orbitwise
+            break;
+
+         case GDCC::IR::Code::OrXU_W:
+            putWord(out, 74); // eorbitwise
+            break;
+
+         case GDCC::IR::Code::Retn:
+            putStmnt_Retn(out, stmnt);
+            break;
+
+         case GDCC::IR::Code::ShLU_W:
+            putWord(out, 76); // lshift
+            break;
+
+         case GDCC::IR::Code::ShRI_W:
+            putWord(out, 77); // rshift
+            break;
+
+         case GDCC::IR::Code::SubI_W:
+         case GDCC::IR::Code::SubU_W:
+            putWord(out, 15); // subtract
+            break;
+
+         case GDCC::IR::Code::Swap_W:
+            putWord(out, 217); // swap
+            break;
+
+         default:
+            std::cerr << "ERROR: " << stmnt.pos << ": cannot put Code for ZDACS: "
+               << stmnt.code << '\n';
+            throw EXIT_FAILURE;
+         }
+      }
+
       //
       // Info::putStmnt_Call
       //
@@ -73,6 +211,229 @@ namespace Bytecode
       }
 
       //
+      // Info::putStmnt_Cspe
+      //
+      void Info::putStmnt_Cspe(std::ostream &out, GDCC::IR::Statement const &stmnt)
+      {
+         auto ret = ResolveValue(stmnt.args[1].aLit.value->getValue());
+
+         GDCC::IR::ArgBase a;
+         if(stmnt.args.size() == 2)
+            a = ret ? GDCC::IR::ArgBase::Stk : GDCC::IR::ArgBase::Lit;
+         else
+            a = stmnt.args[2].a;
+
+         switch(a)
+         {
+         case GDCC::IR::ArgBase::Lit:
+            if(ret)
+            {
+               for(auto const &arg : GDCC::MakeRange(stmnt.args.begin() + 2, stmnt.args.end()))
+               {
+                  putWord(out, 3); // pushnumber
+                  putExpWord(out, arg.aLit.value);
+               }
+
+               putWord(out, 263); break; // lspec5result
+               putExpWord(out, stmnt.args[0].aLit.value);
+
+               break;
+            }
+
+            switch(stmnt.args.size() - 2)
+            {
+            case 0: putWord(out,  9); break; // lspec1direct
+            case 1: putWord(out,  9); break; // lspec1direct
+            case 2: putWord(out, 10); break; // lspec2direct
+            case 3: putWord(out, 11); break; // lspec3direct
+            case 4: putWord(out, 12); break; // lspec4direct
+            case 5: putWord(out, 13); break; // lspec5direct
+            }
+
+            putExpWord(out, stmnt.args[0].aLit.value);
+
+            // Dummy arg.
+            if(stmnt.args.size() == 2)
+               putWord(out, 0);
+
+            for(auto const &arg : GDCC::MakeRange(stmnt.args.begin() + 2, stmnt.args.end()))
+               putExpWord(out, arg.aLit.value);
+
+            break;
+
+         case GDCC::IR::ArgBase::Stk:
+            if(ret)
+            {
+               switch(stmnt.args.size() - 2)
+               {
+               case 0: putWord(out, 3); putWord(out, 0); // pushnumber
+               case 1: putWord(out, 3); putWord(out, 0); // pushnumber
+               case 2: putWord(out, 3); putWord(out, 0); // pushnumber
+               case 3: putWord(out, 3); putWord(out, 0); // pushnumber
+               case 4: putWord(out, 3); putWord(out, 0); // pushnumber
+               case 5: putWord(out, 263); break; // lspec5result
+               }
+            }
+            else
+            {
+               switch(stmnt.args.size() - 2)
+               {
+               case 0: putWord(out, 3); putWord(out, 0); // pushnumber
+               case 1: putWord(out, 4); break; // lspec1
+               case 2: putWord(out, 5); break; // lspec2
+               case 3: putWord(out, 6); break; // lspec3
+               case 4: putWord(out, 7); break; // lspec4
+               case 5: putWord(out, 8); break; // lspec5
+               }
+            }
+
+            putExpWord(out, stmnt.args[0].aLit.value);
+
+            break;
+
+         default:
+            std::cerr << "ERROR: " << stmnt.pos << ": bad Cspe\n";
+            throw EXIT_FAILURE;
+         }
+      }
+
+      //
+      // Info::putStmnt_Move_W
+      //
+      void Info::putStmnt_Move_W(std::ostream &out, GDCC::IR::Statement const &stmnt)
+      {
+         // push_?
+         if(stmnt.args[0].a == GDCC::IR::ArgBase::Stk) switch(stmnt.args[1].a)
+         {
+         case GDCC::IR::ArgBase::GblArr:
+            putStmnt_Move_W__Stk_Arr(out, stmnt.args[1].aGblArr, 235); // pushglobalarray
+            break;
+
+         case GDCC::IR::ArgBase::GblReg:
+            putWord(out, 182); // pushglobalvar
+            putExpWord(out, stmnt.args[1].aGblReg.idx->aLit.value);
+            break;
+
+         case GDCC::IR::ArgBase::Lit:
+            putStmnt_Move_W__Stk_Lit(out, stmnt.args[1].aLit.value);
+            break;
+
+         case GDCC::IR::ArgBase::LocReg:
+            putWord(out, 28); // pushscriptvar
+            putExpWord(out, stmnt.args[1].aLocReg.idx->aLit.value);
+            break;
+
+         case GDCC::IR::ArgBase::MapArr:
+            putStmnt_Move_W__Stk_Arr(out, stmnt.args[1].aMapArr, 207); // pushmaparray
+            break;
+
+         case GDCC::IR::ArgBase::MapReg:
+            putWord(out, 29); // pushmapvar
+            putExpWord(out, stmnt.args[1].aMapReg.idx->aLit.value);
+            break;
+
+         case GDCC::IR::ArgBase::WldArr:
+            putStmnt_Move_W__Stk_Arr(out, stmnt.args[1].aWldArr, 226); // pushworldarray
+            break;
+
+         case GDCC::IR::ArgBase::WldReg:
+            putWord(out, 30); // pushworldvar
+            putExpWord(out, stmnt.args[1].aWldReg.idx->aLit.value);
+            break;
+
+         default:
+            std::cerr << "bad Code::Move_W(Stk, ?)\n";
+            throw EXIT_FAILURE;
+         }
+
+         // drop_?
+         else if(stmnt.args[1].a == GDCC::IR::ArgBase::Stk) switch(stmnt.args[0].a)
+         {
+         case GDCC::IR::ArgBase::GblArr:
+            putStmnt_Move_W__Arr_Stk(out, stmnt.args[1].aGblArr, 236); // assignglobalarray
+            break;
+
+         case GDCC::IR::ArgBase::GblReg:
+            putWord(out, 181); // assignglobalvar
+            putExpWord(out, stmnt.args[0].aGblReg.idx->aLit.value);
+            break;
+
+         case GDCC::IR::ArgBase::LocReg:
+            putWord(out, 25); // assignscriptvar
+            putExpWord(out, stmnt.args[0].aLocReg.idx->aLit.value);
+            break;
+
+         case GDCC::IR::ArgBase::MapArr:
+            putStmnt_Move_W__Arr_Stk(out, stmnt.args[1].aMapArr, 208); // assignmaparray
+            break;
+
+         case GDCC::IR::ArgBase::MapReg:
+            putWord(out, 26); // assignmapvar
+            putExpWord(out, stmnt.args[0].aMapReg.idx->aLit.value);
+            break;
+
+         case GDCC::IR::ArgBase::Nul:
+            putWord(out, 54); // drop
+            break;
+
+         case GDCC::IR::ArgBase::WldArr:
+            putStmnt_Move_W__Arr_Stk(out, stmnt.args[1].aWldArr, 227); // assignworldarray
+            break;
+
+         case GDCC::IR::ArgBase::WldReg:
+            putWord(out, 27); // assignworldvar
+            putExpWord(out, stmnt.args[0].aWldReg.idx->aLit.value);
+            break;
+
+         default:
+            std::cerr << "bad Code::Move_W(?, Stk)\n";
+            throw EXIT_FAILURE;
+         }
+
+         // ???
+         else
+         {
+            std::cerr << "bad Code::Move_W\n";
+            throw EXIT_FAILURE;
+         }
+      }
+
+      //
+      // Info::putStmnt_Move_W__Arr_Stk
+      //
+      void Info::putStmnt_Move_W__Arr_Stk(std::ostream &out,
+         GDCC::IR::ArgPtr2 const &arr, GDCC::FastU i)
+      {
+         if(!IsExp0(arr.off))
+         {
+            putWord(out, 3); // pushnumber
+            putExpWord(out, arr.off);
+            putWord(out, 14); // add
+         }
+
+         putWord(out, 217); // swap
+         putWord(out, i);
+         putExpWord(out, arr.arr->aLit.value);
+      }
+
+      //
+      // Info::putStmnt_Move_W__Stk_Arr
+      //
+      void Info::putStmnt_Move_W__Stk_Arr(std::ostream &out,
+         GDCC::IR::ArgPtr2 const &arr, GDCC::FastU i)
+      {
+         if(!IsExp0(arr.off))
+         {
+            putWord(out, 3); // pushnumber
+            putExpWord(out, arr.off);
+            putWord(out, 14); // add
+         }
+
+         putWord(out, i);
+         putExpWord(out, arr.arr->aLit.value);
+      }
+
+      //
       // Info::putStmnt_Move_W__Stk_Lit
       //
       void Info::putStmnt_Move_W__Stk_Lit(std::ostream &out, GDCC::IR::Exp const *exp)
@@ -96,6 +457,52 @@ namespace Bytecode
             putWord(out, 3); // pushnumber
             putWord(out, ResolveValue(val));
             break;
+         }
+      }
+
+      //
+      // Info::putStmnt_Retn
+      //
+      void Info::putStmnt_Retn(std::ostream &out, GDCC::IR::Statement const &stmnt)
+      {
+         switch(curFunc->ctype)
+         {
+         case GDCC::IR::CallType::LangACS:
+            if(stmnt.args.size() == 0)
+               putWord(out, 205); // returnvoid
+
+            else if(stmnt.args.size() == 1)
+               putWord(out, 206); // returnval
+
+            else
+            {
+               std::cerr << "STUB: " __FILE__ << ':' << __LINE__ << '\n';
+               throw EXIT_FAILURE;
+            }
+            break;
+
+         case GDCC::IR::CallType::Script:
+         case GDCC::IR::CallType::ScriptI:
+         case GDCC::IR::CallType::ScriptS:
+            if(stmnt.args.size() == 0)
+            {
+               putWord(out, 1); // terminate
+            }
+            else if(stmnt.args.size() == 1)
+            {
+               putWord(out, 257); // setresultvalue
+               putWord(out,   1); // terminate
+            }
+            else
+            {
+               std::cerr << "STUB: " __FILE__ << ':' << __LINE__ << '\n';
+               throw EXIT_FAILURE;
+            }
+            break;
+
+         default:
+            std::cerr << "ERROR: " << stmnt.pos << ": bad Code::Retn\n";
+            throw EXIT_FAILURE;
          }
       }
    }
--- src/Bytecode/ZDACS/trStmnt.cpp
+++ src/Bytecode/ZDACS/trStmnt.cpp
@@ -12,7 +12,8 @@
 
 #include "Info.hpp"
 
-#include "GDCC/IR/Statement.hpp"
+#include "GDCC/IR/Function.hpp"
+#include "GDCC/IR/Glyph.hpp"
 
 #include <iostream>
 
@@ -25,6 +26,123 @@ namespace Bytecode
 {
    namespace ZDACS
    {
+      //
+      // Info::trStmnt
+      //
+      void Info::trStmnt(GDCC::IR::Statement &stmnt)
+      {
+         // Generate label glyphs.
+         if(!stmnt.labs.empty())
+         {
+            auto val = GDCC::IR::ExpCreate_ValueRoot(
+               GDCC::IR::Value_Fixed(jumpPos, TypeWord), stmnt.pos);
+
+            for(auto const &lab : stmnt.labs)
+            {
+               auto &data = GDCC::IR::Glyph::GetData(lab);
+
+               data.type  = TypeWord;
+               data.value = val;
+            }
+         }
+
+         switch(stmnt.code)
+         {
+         case GDCC::IR::Code::Nop:
+            jumpPos += 4;
+            break;
+
+         case GDCC::IR::Code::AddI_W:
+         case GDCC::IR::Code::AddU_W:
+         case GDCC::IR::Code::AndU_W:
+         case GDCC::IR::Code::DivI_W:
+         case GDCC::IR::Code::DivX_W:
+         case GDCC::IR::Code::ModI_W:
+         case GDCC::IR::Code::MulI_W:
+         case GDCC::IR::Code::MulX_W:
+         case GDCC::IR::Code::OrIU_W:
+         case GDCC::IR::Code::OrXU_W:
+         case GDCC::IR::Code::ShLU_W:
+         case GDCC::IR::Code::ShRI_W:
+         case GDCC::IR::Code::SubI_W:
+         case GDCC::IR::Code::SubU_W:
+            CheckArgC(stmnt, 3);
+            CheckArgB(stmnt, 0, GDCC::IR::ArgBase::Stk);
+            CheckArgB(stmnt, 1, GDCC::IR::ArgBase::Stk);
+            CheckArgB(stmnt, 2, GDCC::IR::ArgBase::Stk);
+            jumpPos += 4;
+            break;
+
+         case GDCC::IR::Code::Call:
+            trStmnt_Call(stmnt);
+            break;
+
+         case GDCC::IR::Code::Casm:
+            CheckArgC(stmnt, 1);
+            for(auto n = stmnt.args.size(); --n;)
+               CheckArgB(stmnt, n, GDCC::IR::ArgBase::Lit);
+            jumpPos += stmnt.args.size() * 4;
+            break;
+
+         case GDCC::IR::Code::CmpI_EQ_W:
+         case GDCC::IR::Code::CmpI_GE_W:
+         case GDCC::IR::Code::CmpI_GT_W:
+         case GDCC::IR::Code::CmpI_LE_W:
+         case GDCC::IR::Code::CmpI_LT_W:
+         case GDCC::IR::Code::CmpI_NE_W:
+         case GDCC::IR::Code::CmpU_EQ_W:
+         case GDCC::IR::Code::CmpU_NE_W:
+            CheckArgC(stmnt, 3);
+            CheckArgB(stmnt, 0, GDCC::IR::ArgBase::Stk);
+            CheckArgB(stmnt, 1, GDCC::IR::ArgBase::Stk);
+            CheckArgB(stmnt, 2, GDCC::IR::ArgBase::Stk);
+            jumpPos += 4;
+            break;
+
+         case GDCC::IR::Code::Cnat:
+            CheckArgC(stmnt, 2);
+            CheckArgB(stmnt, 0, GDCC::IR::ArgBase::Lit);
+            CheckArgB(stmnt, 1, GDCC::IR::ArgBase::Lit);
+            for(auto n = stmnt.args.size(); n-- != 2;)
+               CheckArgB(stmnt, n, GDCC::IR::ArgBase::Stk);
+            jumpPos += 12;
+            break;
+
+         case GDCC::IR::Code::Cspe:
+            trStmnt_Cspe(stmnt);
+            break;
+
+         case GDCC::IR::Code::InvU_W:
+         case GDCC::IR::Code::NegI_W:
+         case GDCC::IR::Code::NotU_W:
+            CheckArgC(stmnt, 2);
+            CheckArgB(stmnt, 0, GDCC::IR::ArgBase::Stk);
+            CheckArgB(stmnt, 1, GDCC::IR::ArgBase::Stk);
+            jumpPos += 4;
+            break;
+
+         case GDCC::IR::Code::Move_W:
+            trStmnt_Move_W(stmnt);
+            break;
+
+         case GDCC::IR::Code::Retn:
+            trStmnt_Retn(stmnt);
+            break;
+
+         case GDCC::IR::Code::Swap_W:
+            CheckArgC(stmnt, 2);
+            CheckArgB(stmnt, 0, GDCC::IR::ArgBase::Stk);
+            CheckArgB(stmnt, 1, GDCC::IR::ArgBase::Stk);
+            jumpPos += 4;
+            break;
+
+         default:
+            std::cerr << "ERROR: " << stmnt.pos << ": cannot translate Code for ZDACS: "
+               << stmnt.code << '\n';
+            throw EXIT_FAILURE;
+         }
+      }
+
       //
       // Info::trStmnt_Call
       //
@@ -76,6 +194,201 @@ namespace Bytecode
       }
 
       //
+      // Info::trStmnt_Cspe
+      //
+      void Info::trStmnt_Cspe(GDCC::IR::Statement &stmnt)
+      {
+         CheckArgC(stmnt, 2);
+         CheckArgB(stmnt, 0, GDCC::IR::ArgBase::Lit);
+         CheckArgB(stmnt, 1, GDCC::IR::ArgBase::Lit);
+
+         auto ret = ResolveValue(stmnt.args[1].aLit.value->getValue());
+
+         if(ret > 1)
+         {
+            std::cerr << "ERROR: " << stmnt.pos << ": bad Cspe ret\n";
+            throw EXIT_FAILURE;
+         }
+
+         // Too many call args.
+         if(stmnt.args.size() > 7)
+         {
+            std::cerr << "ERROR: " << stmnt.pos << ": bad Cspe arg count\n";
+            throw EXIT_FAILURE;
+         }
+
+         // No call args.
+         if(stmnt.args.size() == 2)
+         {
+            jumpPos += ret ? 48 : 12;
+            return;
+         }
+
+         switch(stmnt.args[2].a)
+         {
+         case GDCC::IR::ArgBase::Lit:
+            for(auto n = stmnt.args.size(); n-- != 3;)
+               CheckArgB(stmnt, n, GDCC::IR::ArgBase::Lit);
+            jumpPos += 8 + (stmnt.args.size() - 2) * (ret ? 8 : 4);
+            break;
+
+         case GDCC::IR::ArgBase::Stk:
+            for(auto n = stmnt.args.size(); n-- != 3;)
+               CheckArgB(stmnt, n, GDCC::IR::ArgBase::Stk);
+            jumpPos += 8;
+
+            // Dummy args.
+            if(ret) jumpPos += (7 - stmnt.args.size()) * 8;
+
+            break;
+
+         default:
+            std::cerr << "ERROR: " << stmnt.pos << ": bad Cspe\n";
+            throw EXIT_FAILURE;
+         }
+      }
+
+      //
+      // Info::trStmnt_Move_W
+      //
+      void Info::trStmnt_Move_W(GDCC::IR::Statement &stmnt)
+      {
+         CheckArgC(stmnt, 2);
+         CheckArg(stmnt.args[0], stmnt.pos);
+         CheckArg(stmnt.args[1], stmnt.pos);
+
+         switch(stmnt.args[0].a)
+         {
+         case GDCC::IR::ArgBase::GblArr:
+            switch(stmnt.args[1].a)
+            {
+            case GDCC::IR::ArgBase::Stk:
+               trStmnt_Move_W__Arr_Stk(stmnt, stmnt.args[0].aGblArr);
+               break;
+
+            default: goto badcase;
+            }
+            break;
+
+         case GDCC::IR::ArgBase::GblReg:
+            switch(stmnt.args[1].a)
+            {
+            case GDCC::IR::ArgBase::Stk: jumpPos += 8; break;
+            default: goto badcase;
+            }
+            break;
+
+         case GDCC::IR::ArgBase::LocReg:
+            switch(stmnt.args[1].a)
+            {
+            case GDCC::IR::ArgBase::Stk: jumpPos += 8; break;
+            default: goto badcase;
+            }
+            break;
+
+         case GDCC::IR::ArgBase::MapArr:
+            switch(stmnt.args[1].a)
+            {
+            case GDCC::IR::ArgBase::Stk:
+               trStmnt_Move_W__Arr_Stk(stmnt, stmnt.args[0].aMapArr);
+               break;
+
+            default: goto badcase;
+            }
+            break;
+
+         case GDCC::IR::ArgBase::MapReg:
+            switch(stmnt.args[1].a)
+            {
+            case GDCC::IR::ArgBase::Stk: jumpPos += 8; break;
+            default: goto badcase;
+            }
+            break;
+
+         case GDCC::IR::ArgBase::Nul:
+            switch(stmnt.args[1].a)
+            {
+            case GDCC::IR::ArgBase::Stk: jumpPos += 4; break;
+            default: goto badcase;
+            }
+            break;
+
+         case GDCC::IR::ArgBase::Stk:
+            switch(stmnt.args[1].a)
+            {
+            case GDCC::IR::ArgBase::GblReg: jumpPos += 8; break;
+            case GDCC::IR::ArgBase::LocReg: jumpPos += 8; break;
+            case GDCC::IR::ArgBase::MapReg: jumpPos += 8; break;
+            case GDCC::IR::ArgBase::WldReg: jumpPos += 8; break;
+
+            case GDCC::IR::ArgBase::Lit:
+               trStmnt_Move_W__Stk_Lit(stmnt, stmnt.args[1].aLit.value);
+               break;
+
+            case GDCC::IR::ArgBase::GblArr:
+               trStmnt_Move_W__Stk_Arr(stmnt, stmnt.args[1].aGblArr);
+               break;
+
+            case GDCC::IR::ArgBase::MapArr:
+               trStmnt_Move_W__Stk_Arr(stmnt, stmnt.args[1].aMapArr);
+               break;
+
+            case GDCC::IR::ArgBase::WldArr:
+               trStmnt_Move_W__Stk_Arr(stmnt, stmnt.args[1].aWldArr);
+               break;
+
+            default: goto badcase;
+            }
+            break;
+
+         case GDCC::IR::ArgBase::WldArr:
+            switch(stmnt.args[1].a)
+            {
+            case GDCC::IR::ArgBase::Stk:
+               trStmnt_Move_W__Arr_Stk(stmnt, stmnt.args[0].aWldArr);
+               break;
+
+            default: goto badcase;
+            }
+            break;
+
+         case GDCC::IR::ArgBase::WldReg:
+            switch(stmnt.args[1].a)
+            {
+            case GDCC::IR::ArgBase::Stk: jumpPos += 8; break;
+            default: goto badcase;
+            }
+            break;
+
+         default:
+         badcase:
+            std::cerr << "ERROR: " << stmnt.pos << ": bad Code::Move_W("
+               << stmnt.args[0].a << ',' << stmnt.args[1].a << ")\n";
+            throw EXIT_FAILURE;
+         }
+      }
+
+      //
+      // Info::trStmnt_Move_W__Arr_Stk
+      //
+      void Info::trStmnt_Move_W__Arr_Stk(GDCC::IR::Statement &stmnt, GDCC::IR::ArgPtr2 const &arr)
+      {
+         CheckArgB(*arr.idx, GDCC::IR::ArgBase::Stk, stmnt.pos);
+
+         jumpPos += IsExp0(arr.off) ? 24 : 12;
+      }
+
+      //
+      // Info::trStmnt_Move_W__Stk_Arr
+      //
+      void Info::trStmnt_Move_W__Stk_Arr(GDCC::IR::Statement &stmnt, GDCC::IR::ArgPtr2 const &arr)
+      {
+         CheckArgB(*arr.idx, GDCC::IR::ArgBase::Stk, stmnt.pos);
+
+         jumpPos += IsExp0(arr.off) ? 20 : 8;
+      }
+
+      //
       // Info::trStmnt_Move_W__Stk_Lit
       //
       void Info::trStmnt_Move_W__Stk_Lit(GDCC::IR::Statement &, GDCC::IR::Exp const *exp)
@@ -88,6 +401,48 @@ namespace Bytecode
          case GDCC::IR::TypeBase::StrEn: jumpPos += 12; break;
 
          default: jumpPos += 8; break;
+         }
+      }
+
+      //
+      // Info::trStmnt_Retn
+      //
+      void Info::trStmnt_Retn(GDCC::IR::Statement &stmnt)
+      {
+         for(auto n = stmnt.args.size(); n--;)
+            CheckArgB(stmnt, n, GDCC::IR::ArgBase::Stk);
+
+         switch(curFunc->ctype)
+         {
+         case GDCC::IR::CallType::LangACS:
+            if(stmnt.args.size() == 0)
+               jumpPos += 4;
+            else if(stmnt.args.size() == 1)
+               jumpPos += 4;
+            else
+            {
+               std::cerr << "STUB: " __FILE__ << ':' << __LINE__ << '\n';
+               throw EXIT_FAILURE;
+            }
+            break;
+
+         case GDCC::IR::CallType::Script:
+         case GDCC::IR::CallType::ScriptI:
+         case GDCC::IR::CallType::ScriptS:
+            if(stmnt.args.size() == 0)
+               jumpPos += 4;
+            else if(stmnt.args.size() == 1)
+               jumpPos += 8;
+            else
+            {
+               std::cerr << "STUB: " __FILE__ << ':' << __LINE__ << '\n';
+               throw EXIT_FAILURE;
+            }
+            break;
+
+         default:
+            std::cerr << "ERROR: " << stmnt.pos << ": bad Code::Retn\n";
+            throw EXIT_FAILURE;
          }
       }
    }
