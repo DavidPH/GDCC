42c497e7a5dddfffc2ff3b0e38a71a22b5faea91
David Hill <DavidPH55@verizon.net>
2014-03-02 10:53:28 -0500

    CC: Implemented ExpTo* functions.

--- src/CC/Exp.cpp
+++ src/CC/Exp.cpp
@@ -16,6 +16,11 @@
 #include "AST/Exp.hpp"
 #include "AST/Type.hpp"
 
+#include "Core/Exception.hpp"
+
+#include "IR/Exp.hpp"
+#include "IR/Value.hpp"
+
 
 //----------------------------------------------------------------------------|
 // Global Functions                                                           |
@@ -25,6 +30,38 @@ namespace GDCC
 {
    namespace CC
    {
+      //
+      // ExpToFastU
+      //
+      Core::FastU ExpToFastU(AST::Exp const *exp)
+      {
+         auto i = ExpToInteg(exp);
+
+         // TODO: Check range?
+
+         return number_cast<Core::FastU>(i);
+      }
+
+      //
+      // ExpToInteg
+      //
+      Core::Integ ExpToInteg(AST::Exp const *exp)
+      {
+         auto val = exp->getIRExp()->getValue();
+
+         switch(val.v)
+         {
+         case IR::ValueBase::Fixed:
+            return val.vFixed.value >> val.vFixed.vtype.bitsF;
+
+         case IR::ValueBase::Float:
+            return number_cast<Core::Integ>(val.vFloat.value);
+
+         default:
+            throw Core::ExceptStr(exp->pos, "invalid integer constant");
+         }
+      }
+
       //
       // IsLValue
       //
