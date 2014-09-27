716531bda875bc5e798bd3982c96fc499f60be1f
David Hill <DavidPH55@verizon.net>
2014-09-27 18:22:49 -0400

    libGDCC: Added libGDCC target library.
    This library implements basic runtime behavior needed by codegen and translation. That is, this library is an implicit dependency for any project compiled with GDCC, regardless of language.
    Initial component is memory allocation, which will be needed for the automatic storage implementation.

---
+++ lib/inc/C/GDCC.h
@@ -0,0 +1,45 @@
+//-----------------------------------------------------------------------------
+//
+// Copyright(C) 2014 David Hill
+//
+// See COPYLIB for license information.
+//
+//-----------------------------------------------------------------------------
+//
+// Header for target library libGDCC.
+//
+// This header declares functions, objects, and types which are specific and
+// possibly internal to GDCC. Neither the contents or existence of this file
+// should be relied upon by external projects.
+//
+//-----------------------------------------------------------------------------
+
+#ifndef __GDCC_Header__C_GDCC_h__
+#define __GDCC_Header__C_GDCC_h__
+
+
+//----------------------------------------------------------------------------|
+// Types                                                                      |
+//
+
+#ifndef __GDCC_Have__size_t__
+#define __GDCC_Have__size_t__
+typedef unsigned size_t;
+#endif
+
+#ifndef __GDCC_Have__ssize_t__
+#define __GDCC_Have__ssize_t__
+typedef int ssize_t;
+#endif
+
+
+//----------------------------------------------------------------------------|
+// Global Functions                                                           |
+//
+
+extern void __loc_ars *__GDCC__alloc(void __loc_ars *ptr, size_t size);
+
+extern void __GDCC__alloc_dump(void);
+
+#endif//__GDCC_Header__C_GDCC_h__
+
---
+++ lib/src/libGDCC/alloc.c
@@ -0,0 +1,377 @@
+//-----------------------------------------------------------------------------
+//
+// Copyright(C) 2014 David Hill
+//
+// See COPYLIB for license information.
+//
+//-----------------------------------------------------------------------------
+//
+// Memory allocation routines.
+//
+//-----------------------------------------------------------------------------
+
+#include <GDCC.h>
+
+
+//----------------------------------------------------------------------------|
+// Macros                                                                     |
+//
+
+//
+// __GDCC__AllocSize
+//
+// Controls the total size of the allocation heap, in bytes.
+//
+#ifndef __GDCC__AllocSize
+#define __GDCC__AllocSize (16*1024*1024)
+#endif
+
+//
+// __GDCC__MinSplit
+//
+// Blocks will not be split if the remainder would be smaller than this.
+//
+#ifndef __GDCC__MinSplit
+#define __GDCC__MinSplit (sizeof(MemBlock) * 2 + sizeof(int) * 4)
+#endif
+
+//
+// MemBlock_ClrUsed
+//
+#define MemBlock_ClrUsed(block) ((block)->flag = 0)
+
+//
+// MemBlock_IsUsed
+//
+#define MemBlock_IsUsed(block) ((block)->flag)
+
+//
+// MemBlock_SetUsed
+//
+#define MemBlock_SetUsed(block) ((block)->flag = 1)
+
+//
+// PtrToBlock
+//
+#define PtrToBlock(ptr) ((MemBlockPtr)((CharPtr)(ptr) - sizeof(MemBlock)))
+
+
+//----------------------------------------------------------------------------|
+// Types                                                                      |
+//
+
+struct MemBlock;
+
+typedef struct MemBlock MemBlock;
+
+typedef MemBlock __loc_ars *MemBlockPtr;
+typedef char     __loc_ars *CharPtr;
+typedef void     __loc_ars *VoidPtr;
+
+//
+// MemBlock
+//
+struct MemBlock
+{
+   MemBlockPtr next, prev;
+
+   size_t size;
+   size_t flag;
+
+   char data[];
+};
+
+
+//----------------------------------------------------------------------------|
+// Static Variables                                                           |
+//
+
+//_Alignas(MemBlock)
+static char AllocHeapRaw[__GDCC__AllocSize];
+
+static MemBlockPtr AllocBase, AllocIter;
+
+
+//----------------------------------------------------------------------------|
+// Static Functions                                                           |
+//
+
+//
+// AllocDel
+//
+static void AllocDel(register MemBlockPtr block)
+{
+   register MemBlockPtr next = block->next;
+   register MemBlockPtr prev = block->prev;
+
+   // Determine which neighbors are free and can be merged.
+   register int nextFree = next  != AllocBase && !MemBlock_IsUsed(next);
+   register int prevFree = block != AllocBase && !MemBlock_IsUsed(prev);
+
+   if(prevFree)
+   {
+      // Both neighbors free.
+      if(nextFree)
+      {
+         if(AllocIter == block || AllocIter == next)
+            AllocIter = prev;
+
+         (prev->next = next->next)->prev = prev;
+
+         prev->size += block->size + next->size + sizeof(MemBlock) * 2;
+      }
+
+      // Only prev free.
+      else
+      {
+         if(AllocIter == block)
+            AllocIter = prev;
+
+         prev->next = next;
+         next->prev = prev;
+
+         prev->size += block->size + sizeof(MemBlock);
+      }
+   }
+   else
+   {
+      // Only next free.
+      if(nextFree)
+      {
+         if(AllocIter == next)
+            AllocIter = prev;
+
+         (block->next = next->next)->prev = block;
+
+         block->size += next->size + sizeof(MemBlock);
+
+         MemBlock_ClrUsed(block);
+      }
+
+      // No neighbor free.
+      else
+      {
+         MemBlock_ClrUsed(block);
+      }
+   }
+}
+
+//
+// AllocInit
+//
+static void AllocInit(void)
+{
+   AllocBase = AllocIter = (MemBlockPtr)AllocHeapRaw;
+
+   AllocBase->next = AllocBase->prev = AllocBase;
+   AllocBase->size = __GDCC__AllocSize - sizeof(MemBlock);
+   AllocBase->flag = 0;
+}
+
+//
+// AllocMerge
+//
+static _Bool AllocMerge(register MemBlockPtr block, register size_t size)
+{
+   // Check if next block is free and contains enough space.
+   register MemBlockPtr blockNext = block->next;
+   if(blockNext == AllocBase || MemBlock_IsUsed(blockNext))
+      return 0;
+
+   // Would the combined block have enough space?
+   register size_t sizeFull = block->size + blockNext->size + sizeof(MemBlock);
+   if((ssize_t)sizeFull < (ssize_t)size)
+      return 0;
+
+   register MemBlockPtr blockNextNext = blockNext->next;
+   register size_t      sizeDiff      = sizeFull - size;
+
+   if(AllocIter == blockNext)
+      AllocIter = block;
+
+   // Big enough to split.
+   if((ssize_t)sizeDiff > (ssize_t)__GDCC__MinSplit)
+   {
+      // Place new block at end of current block.
+      blockNext = (MemBlockPtr)(block->data + size);
+
+      // Initialize new block.
+      blockNext->prev = block;
+      blockNext->next = block->next;
+      blockNext->size = sizeDiff - sizeof(MemBlock);
+      blockNext->flag = 0;
+
+      // Update neighboring blocks.
+      block->size         = size;
+      block->next         = blockNext;
+      blockNextNext->prev = blockNext;
+   }
+
+   // No, just merge.
+   else
+   {
+      block->size         = sizeFull;
+      block->next         = blockNextNext;
+      blockNextNext->prev = block;
+   }
+
+   return 1;
+}
+
+//
+// AllocNew
+//
+static VoidPtr AllocNew(register size_t size)
+{
+   // TODO: Round size up to alignment of MemBlock.
+
+   register MemBlockPtr iter = AllocIter;
+
+   do
+   {
+      // Skip used blocks.
+      if(MemBlock_IsUsed(iter)) continue;
+
+      // Exact size match!
+      if(iter->size == size)
+      {
+         MemBlock_SetUsed(iter);
+
+         AllocIter = iter->next;
+         return iter->data;
+      }
+
+      // Bigger, possibly split?
+      if((ssize_t)iter->size > (ssize_t)size)
+      {
+         register size_t sizeDiff = iter->size - size;
+
+         // Big enough to split.
+         if((ssize_t)sizeDiff > (ssize_t)__GDCC__MinSplit)
+         {
+            // Place new block at end of current block.
+            MemBlockPtr newBlock = PtrToBlock(iter->data + sizeDiff);
+
+            // Initialize new block.
+            newBlock->prev = iter;
+            newBlock->next = iter->next;
+            newBlock->size = size;
+            newBlock->flag = 1;
+
+            // Update neighboring blocks.
+            iter->size       = sizeDiff - sizeof(MemBlock);
+            iter->next->prev = newBlock;
+            iter->next       = newBlock;
+
+            // Return allocation.
+            AllocIter = iter;
+            return newBlock->data;
+         }
+
+         // No, just use it as-is.
+         else
+         {
+            MemBlock_SetUsed(iter);
+
+            AllocIter = iter->next;
+            return iter->data;
+         }
+      }
+
+      // Not big enough, move along.
+   }
+   while((iter = iter->next) != AllocIter);
+
+   // No space found, give up.
+   return 0;
+}
+
+
+//----------------------------------------------------------------------------|
+// Global Functions                                                           |
+//
+
+//
+// __GDCC__alloc
+//
+VoidPtr __GDCC__alloc(register VoidPtr ptrOld, register size_t size)
+{
+   if(!AllocIter) AllocInit();
+
+   // No existing allocation.
+   if(!ptrOld)
+      return size ? AllocNew(size) : 0;
+
+   register MemBlockPtr block = PtrToBlock(ptrOld);
+
+   // Size 0 means free.
+   if(!size)
+      return AllocDel(block), 0;
+
+   // If block already has enough space, then no need to realloc.
+   if((ssize_t)block->size >= (ssize_t)size)
+      return ptrOld;
+
+   // Try a merging block expansion.
+   if(AllocMerge(block, size))
+      return ptrOld;
+
+   // Fallback to simply allocate new block and memcpy.
+   register CharPtr ptrNew = AllocNew(size);
+
+   if(!ptrNew) return 0;
+
+   for(register CharPtr itrNew = ptrNew, itrOld = ptrOld,
+      end = itrOld + block->size; itrOld != end;)
+      *itrNew++ = *itrOld++;
+
+   AllocDel(block);
+   return ptrNew;
+}
+
+//
+// __GDCC__alloc_dump
+//
+void __GDCC__alloc_dump(void)
+{
+   if(!AllocIter) AllocInit();
+
+   #if __GDCC_Target__ZDoom__
+   [[address(85), call("AsmFunc")]]
+   extern void ACS_BeginPrint(void);
+
+   [[address(89), call("AsmFunc")]]
+   extern void ACS_PrintChr(char);
+
+   [[address(270), call("AsmFunc")]]
+   extern void ACS_EndLog(void);
+
+   [[address(350), call("AsmFunc")]]
+   extern void ACS_PrintHex(unsigned);
+   [[address(350), call("AsmFunc")]]
+   extern void ACS_PrintPtr(VoidPtr);
+
+   MemBlockPtr iter = AllocBase;
+
+   do
+   {
+      ACS_BeginPrint();
+
+      ACS_PrintPtr(iter);
+      ACS_PrintChr(' ');
+      ACS_PrintPtr(iter->next);
+      ACS_PrintChr(' ');
+      ACS_PrintPtr(iter->prev);
+      ACS_PrintChr(' ');
+      ACS_PrintHex(iter->size);
+      ACS_PrintChr(' ');
+      ACS_PrintHex(iter->flag);
+
+      ACS_EndLog();
+   }
+   while((iter = iter->next) != AllocBase);
+   #endif
+}
+
+// EOF
+
