1b43319bb37e0a06be03d4501690e83fd0dc3157
David Hill <DavidPH55@verizon.net>
2014-11-20 09:41:00 -0500

    AST: Moved GenStmnt/Move.cpp statics into namespace.

--- src/AST/GenStmnt/Move.cpp
+++ src/AST/GenStmnt/Move.cpp
@@ -18,166 +18,148 @@
 
 #include "IR/ExpCode.hpp"
 
-#include <iostream>
-
 
 //----------------------------------------------------------------------------|
 // Static Functions                                                           |
 //
 
-//
-// GenStmnt_MovePartIdx
-//
-template<typename ArgT, typename IdxT>
-static void GenStmnt_MovePartIdx(GDCC::AST::Exp const *exp,
-   GDCC::AST::GenStmntCtx const &ctx, GDCC::AST::Arg const &arg,
-   IdxT const &idx, bool get, bool set)
+namespace GDCC
 {
-   using namespace GDCC;
-
-   if(set)
+   namespace AST
    {
-      for(Core::FastU n = arg.type->getSizeWords(); n--;)
-         AST::GenStmnt_MoveWordSetT<ArgT>(exp, ctx, arg, idx, n);
-   }
-
-   if(get)
-   {
-      for(Core::FastU n = 0, e = arg.type->getSizeWords(); n != e; ++n)
-         AST::GenStmnt_MoveWordGetT<ArgT>(exp, ctx, arg, idx, n);
-   }
-}
-
-//
-// GenStmnt_MovePartT
-//
-template<typename ArgT>
-static void GenStmnt_MovePartT(GDCC::AST::Exp const *exp,
-   GDCC::AST::GenStmntCtx const &ctx, GDCC::AST::Arg const &arg,
-   bool get, bool set)
-{
-   using namespace GDCC;
-
-   // If arg address is a constant, then use Arg_Lit address.
-   if(arg.data->isIRExp())
-   {
-      // Evaluate arg's data for side effects.
-      arg.data->genStmnt(ctx);
-
-      // Use literal as index.
-      GenStmnt_MovePartIdx<ArgT>(exp, ctx, arg,
-         IR::Arg_Lit(arg.data->getIRExp()), get, set);
-
-      return;
-   }
-
-   // If fetching or setting a single word, and not both, use address on stack.
-   if(arg.type->getSizeWords() == 1 && get ^ set)
-   {
-      // Evaluate arg's data.
-      arg.data->genStmntStk(ctx);
-
-      // Use Stk as index.
-      GenStmnt_MovePartIdx<ArgT>(exp, ctx, arg, IR::Arg_Stk(), get, set);
-
-      return;
-   }
-
-   // As a last resort, just evaluate the pointer and store in a temporary.
-   {
-      // Evaluate arg's data.
-      arg.data->genStmntStk(ctx);
-
-      // Move to temporary.
-      AST::Temporary tmp{ctx, exp->pos, arg.data->getType()->getSizeWords()};
-      for(Core::FastU n = tmp.size(); n--;)
-         ctx.block.addStatementArgs(IR::Code::Move_W,
-            tmp.getArg(n), IR::Arg_Stk());
-
-      // Use temporary as index.
-      GenStmnt_MovePartIdx<ArgT>(exp, ctx, arg, tmp.getArg(), get, set);
-
-      return;
-   }
-}
-
-//
-// GenStmnt_MovePartT<IR::Arg_Cpy>
-//
-template<> void GenStmnt_MovePartT<GDCC::IR::Arg_Cpy>(
-   GDCC::AST::Exp const *exp, GDCC::AST::GenStmntCtx const &,
-   GDCC::AST::Arg const &, bool get, bool set)
-{
-   if(set) throw GDCC::Core::ExceptStr(exp->pos, "AddrBase::Cpy set");
-   if(get) throw GDCC::Core::ExceptStr(exp->pos, "AddrBase::Cpy get");
-}
-
-//
-// GenStmnt_MovePartT<IR::Arg_Lit>
-//
-template<> void GenStmnt_MovePartT<GDCC::IR::Arg_Lit>(
-   GDCC::AST::Exp const *exp, GDCC::AST::GenStmntCtx const &ctx,
-   GDCC::AST::Arg const &arg, bool get, bool set)
-{
-   using namespace GDCC;
-
-   if(set) throw Core::ExceptStr(exp->pos, "AddrBase::Lit set");
-
-   if(get)
-   {
-      if(arg.data->isIRExp())
+      //
+      // GenStmnt_MovePartIdx
+      //
+      template<typename ArgT, typename IdxT>
+      static void GenStmnt_MovePartIdx(Exp const *exp, GenStmntCtx const &ctx,
+         Arg const &arg, IdxT const &idx, bool get, bool set)
       {
-         arg.data->genStmnt(ctx);
-
-         auto code = IR::ExpCode_Move(arg.type->getSizeWords());
-
-         if(code == IR::Code::None)
+         if(set)
          {
-            std::cerr << "STUB: " << exp->pos << ": " << __FILE__ << ':'
-               << __LINE__ << ": AddrBase::Lit get multi-word > 3\n";
-            throw EXIT_FAILURE;
+            for(Core::FastU n = arg.type->getSizeWords(); n--;)
+               GenStmnt_MoveWordSetT<ArgT>(exp, ctx, arg, idx, n);
          }
 
-         ctx.block.addStatementArgs(code, IR::Arg_Stk(), arg.data->getIRExp());
+         if(get)
+         {
+            for(Core::FastU n = 0, e = arg.type->getSizeWords(); n != e; ++n)
+               GenStmnt_MoveWordGetT<ArgT>(exp, ctx, arg, idx, n);
+         }
       }
-      else
-         arg.data->genStmnt(ctx, AST::Arg(arg.type, IR::AddrBase::Stk));
-   }
-}
 
-//
-// GenStmnt_MovePartT<IR::Arg_Nul>
-//
-template<> void GenStmnt_MovePartT<GDCC::IR::Arg_Nul>(
-   GDCC::AST::Exp const *exp, GDCC::AST::GenStmntCtx const &ctx,
-   GDCC::AST::Arg const &arg, bool get, bool set)
-{
-   using namespace GDCC;
+      //
+      // GenStmnt_MovePartT
+      //
+      template<typename ArgT>
+      static void GenStmnt_MovePartT(Exp const *exp, GenStmntCtx const &ctx,
+         Arg const &arg, bool get, bool set)
+      {
+         // If arg address is a constant, then use Arg_Lit address.
+         if(arg.data->isIRExp())
+         {
+            // Evaluate arg's data for side effects.
+            arg.data->genStmnt(ctx);
 
-   if(set)
-   {
-      for(Core::FastU n = arg.type->getSizeWords(); n--;)
-         ctx.block.addStatementArgs(IR::Code::Move_W,
-            IR::Arg_Nul(), IR::Arg_Stk());
-   }
+            // Use literal as index.
+            GenStmnt_MovePartIdx<ArgT>(exp, ctx, arg,
+               IR::Arg_Lit(arg.data->getIRExp()), get, set);
 
-   if(get) throw Core::ExceptStr(exp->pos, "AddrBase::Nul get");
-}
+            return;
+         }
 
-//
-// GenStmnt_MovePartT<IR::Arg_Stk>
-//
-template<> void GenStmnt_MovePartT<GDCC::IR::Arg_Stk>(
-   GDCC::AST::Exp const *exp, GDCC::AST::GenStmntCtx const &,
-   GDCC::AST::Arg const &, bool get, bool set)
-{
-   using namespace GDCC;
+         // If fetching or setting a single word, use address on stack.
+         if(arg.type->getSizeWords() == 1 && get ^ set)
+         {
+            // Evaluate arg's data.
+            arg.data->genStmntStk(ctx);
 
-   if(get && set)
-   {
-      std::cerr << "STUB: " << exp->pos << ": " << __FILE__ << ':' << __LINE__
-         << ": AddrBase::Stk get && set\n";
-      throw EXIT_FAILURE;
+            // Use Stk as index.
+            GenStmnt_MovePartIdx<ArgT>(exp, ctx, arg, IR::Arg_Stk(), get, set);
+
+            return;
+         }
+
+         // As a last resort, evaluate the pointer and store in a temporary.
+         {
+            // Evaluate arg's data.
+            arg.data->genStmntStk(ctx);
+
+            // Move to temporary.
+            Temporary tmp{ctx, exp->pos, arg.data->getType()->getSizeWords()};
+            for(Core::FastU n = tmp.size(); n--;)
+               ctx.block.addStatementArgs(IR::Code::Move_W,
+                  tmp.getArg(n), IR::Arg_Stk());
+
+            // Use temporary as index.
+            GenStmnt_MovePartIdx<ArgT>(exp, ctx, arg, tmp.getArg(), get, set);
+
+            return;
+         }
+      }
+
+      //
+      // GenStmnt_MovePartT<IR::Arg_Cpy>
+      //
+      template<> void GenStmnt_MovePartT<IR::Arg_Cpy>(Exp const *exp,
+         GenStmntCtx const &, Arg const &, bool get, bool set)
+      {
+         if(set) throw Core::ExceptStr(exp->pos, "AddrBase::Cpy set");
+         if(get) throw Core::ExceptStr(exp->pos, "AddrBase::Cpy get");
+      }
+
+      //
+      // GenStmnt_MovePartT<IR::Arg_Lit>
+      //
+      template<> void GenStmnt_MovePartT<IR::Arg_Lit>(Exp const *exp,
+         GenStmntCtx const &ctx, Arg const &arg, bool get, bool set)
+      {
+         if(set) throw Core::ExceptStr(exp->pos, "AddrBase::Lit set");
+
+         if(get)
+         {
+            if(arg.data->isIRExp())
+            {
+               arg.data->genStmnt(ctx);
+
+               auto code = IR::ExpCode_Move(arg.type->getSizeWords());
+
+               if(code == IR::Code::None)
+                  throw Core::ExceptStr(exp->pos, "unsupported literal size");
+
+               ctx.block.addStatementArgs(code,
+                  IR::Arg_Stk(), arg.data->getIRExp());
+            }
+            else
+               arg.data->genStmnt(ctx, Arg(arg.type, IR::AddrBase::Stk));
+         }
+      }
+
+      //
+      // GenStmnt_MovePartT<IR::Arg_Nul>
+      //
+      template<> void GenStmnt_MovePartT<IR::Arg_Nul>(Exp const *exp,
+         GenStmntCtx const &ctx, Arg const &arg, bool get, bool set)
+      {
+         if(set)
+         {
+            for(Core::FastU n = arg.type->getSizeWords(); n--;)
+               ctx.block.addStatementArgs(IR::Code::Move_W,
+                  IR::Arg_Nul(), IR::Arg_Stk());
+         }
+
+         if(get) throw Core::ExceptStr(exp->pos, "AddrBase::Nul get");
+      }
+
+      //
+      // GenStmnt_MovePartT<IR::Arg_Stk>
+      //
+      template<> void GenStmnt_MovePartT<IR::Arg_Stk>(
+         Exp const *exp, GenStmntCtx const &,
+         Arg const &, bool get, bool set)
+      {
+         if(get && set)
+            throw Core::ExceptStr(exp->pos, "AddrBase::Stk get && set");
+      }
    }
 }
 
