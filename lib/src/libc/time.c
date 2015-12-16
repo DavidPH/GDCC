03a0e69fe0e11eb6b9cbe3cb9fe785fd0bf301d0
David Hill <DavidPH55@verizon.net>
2015-12-16 17:40:14 -0500

    libc: Fixed TimeCount declaration.

--- lib/src/libc/time.c
+++ lib/src/libc/time.c
@@ -32,7 +32,7 @@ static time_t TimeBase;
 // TimeCount
 //
 #if __GDCC_Family__ZDACS__
-[[addres(999), call("ScriptI"), script("Open")]]
+[[address(999), call("ScriptI"), script("Open")]]
 static void TimeCount(void)
 {
    for(;;)
