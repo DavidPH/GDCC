c0d27a50bdd1ba6647a285f0ef902b87773d6842
David Hill <DavidPH55@verizon.net>
2016-01-04 09:39:59 -0500

    libacs: Updated headers.

--- lib/inc/C/ACS_ZDoom/Native.h
+++ lib/inc/C/ACS_ZDoom/Native.h
@@ -159,6 +159,7 @@ ACS_NativeDeclFull( 89, 0, int,    ChangeActorRoll, int, _Accum, int);
 ACS_NativeDeclFull( 90, 0, _Accum, GetActorRoll, int);
 ACS_NativeDeclFull( 91, 4, int,    QuakeEx, int, int, int, int, int, int, int, __str, int, _Accum, _Accum, _Accum);
 ACS_NativeDeclFull( 92, 3, int,    Warp, int, _Accum, _Accum, _Accum, _Accum, int, __str, int, _Accum);
+ACS_NativeDeclFull( 93, 0, int,    GetMaxInventory, int, __str);
 
 #endif//__GDCC_Header__C__ACS_ZDoom__Native_h__
 
