39576dfca3e1c4f1cab021be531b1dc62ec419ca
David Hill <DavidPH55@verizon.net>
2016-02-26 13:15:29 -0500

    libacs: Add new natives.

--- lib/inc/C/ACS_ZDoom/Native.h
+++ lib/inc/C/ACS_ZDoom/Native.h
@@ -160,6 +160,9 @@ ACS_NativeDeclFull( 90, 0, _Accum, GetActorRoll, int);
 ACS_NativeDeclFull( 91, 4, int,    QuakeEx, int, int, int, int, int, int, int, __str, int, _Accum, _Accum, _Accum);
 ACS_NativeDeclFull( 92, 3, int,    Warp, int, _Accum, _Accum, _Accum, _Accum, int, __str, int, _Accum);
 ACS_NativeDeclFull( 93, 0, int,    GetMaxInventory, int, __str);
+ACS_NativeDeclFull( 94, 3, int,    SetSectorDamage, int, int, __str, int, int);
+ACS_NativeDeclFull( 95, 0, int,    SetSectorTerrain, int, int, __str);
+ACS_NativeDeclFull( 96,14, int,    SpawnParticle, int, int, int, int, _Accum, _Accum, _Accum, _Accum, _Accum, _Accum, _Accum, _Accum, _Accum, int, int);
 
 #endif//__GDCC_Header__C__ACS_ZDoom__Native_h__
 
