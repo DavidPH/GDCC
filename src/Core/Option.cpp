416135c65beb7510f2fc2bdf6832891170bda66e
David Hill <DavidPH55@verizon.net>
2016-02-29 09:00:52 -0500

    Version 0.9.0.

--- src/Core/Option.cpp
+++ src/Core/Option.cpp
@@ -191,7 +191,7 @@ namespace GDCC
          bool needOutput)
       {
          if(!opts.list.version)
-            opts.list.version = "v0.8.1";
+            opts.list.version = "v0.9.0";
 
          if(argc <= 1)
          {
