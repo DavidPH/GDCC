b87bdcae880db0d009a7f0a160f81ce3c2f5ae02
David Hill <DavidPH55@verizon.net>
2016-07-17 09:35:44 -0400

    CC: Add bitfield write for non-IR Arg destination.

--- src/CC/Exp/Assign.cpp
+++ src/CC/Exp/Assign.cpp
@@ -18,7 +18,119 @@
 #include "IR/Exp.hpp"
 
 #include "SR/Arg.hpp"
+#include "SR/GenStmnt/Move.hpp"
+#include "SR/Temporary.hpp"
 #include "SR/Type.hpp"
+
+
+//----------------------------------------------------------------------------|
+// Static Functions                                                           |
+//
+
+namespace GDCC
+{
+   namespace CC
+   {
+      //
+      // GenStmnt_SetBitsPartIdx
+      //
+      template<typename ArgT, typename IdxT>
+      static void GenStmnt_SetBitsPartIdx(SR::Exp const *exp,
+         SR::GenStmntCtx const &ctx, SR::Arg const &arg, IdxT &&idx,
+         Core::FastU bits, Core::FastU offs)
+      {
+         ctx.block.addStatementArgs({IR::Code::Bset_W, arg.type->getSizeWords()},
+            SR::GenStmnt_Move_GenArg<ArgT>(exp, ctx, arg, std::forward<IdxT>(idx), 0),
+            IR::Arg_Stk(), bits, offs);
+      }
+
+      //
+      // GenStmnt_SetBitsPartT
+      //
+      template<typename ArgT>
+      static void GenStmnt_SetBitsPartT(SR::Exp const *exp,
+         SR::GenStmntCtx const &ctx, SR::Arg const &arg, Core::FastU bits,
+         Core::FastU offs)
+      {
+         // If arg address is a constant, then use Arg_Lit address.
+         if(arg.data->isIRExp())
+         {
+            // Evaluate arg's data for side effects.
+            arg.data->genStmnt(ctx);
+
+            // Use literal as index.
+            GenStmnt_SetBitsPartIdx<ArgT>(exp, ctx, arg,
+               IR::Arg_Lit(arg.data->getIRExp()), bits, offs);
+
+            return;
+         }
+
+         // If arg address is an IR arg, use it.
+         // Note that isIRArg implies a lack of side effects.
+         if(arg.data->getArgSrc().isIRArg())
+         {
+            GenStmnt_SetBitsPartIdx<ArgT>(exp, ctx, arg,
+               arg.data->getArgSrc().getIRArg(ctx.prog), bits, offs);
+
+            return;
+         }
+
+         // As a last resort, evaluate the pointer and store in a temporary.
+         {
+            // Evaluate arg's data.
+            arg.data->genStmntStk(ctx);
+
+            // Move to temporary.
+            SR::Temporary tmp{ctx, exp->pos, arg.data->getType()->getSizeWords()};
+            ctx.block.addStatementArgs({IR::Code::Move_W, tmp.size()},
+               tmp.getArg(), IR::Arg_Stk());
+
+            // Use temporary as index.
+            GenStmnt_SetBitsPartIdx<ArgT>(exp, ctx, arg, tmp.getArg(), bits, offs);
+
+            return;
+         }
+      }
+
+      //
+      // GenStmnt_SetBitsPartT<IR::Arg_Cpy>
+      //
+      template<> void GenStmnt_SetBitsPartT<IR::Arg_Cpy>(SR::Exp const *exp,
+         SR::GenStmntCtx const &, SR::Arg const &, Core::FastU, Core::FastU)
+      {
+         throw Core::ExceptStr(exp->pos, "AddrBase::Cpy set");
+      }
+
+      //
+      // GenStmnt_SetBitsPartT<IR::Arg_Lit>
+      //
+      template<> void GenStmnt_SetBitsPartT<IR::Arg_Lit>(SR::Exp const *exp,
+         SR::GenStmntCtx const &, SR::Arg const &, Core::FastU, Core::FastU)
+      {
+         throw Core::ExceptStr(exp->pos, "AddrBase::Lit set");
+      }
+
+      //
+      // GenStmnt_SetBitsPartT<IR::Arg_Nul>
+      //
+      template<> void GenStmnt_SetBitsPartT<IR::Arg_Nul>(SR::Exp const *,
+         SR::GenStmntCtx const &ctx, SR::Arg const &arg, Core::FastU, Core::FastU)
+      {
+         ctx.block.addStatementArgs(
+            {IR::Code::Move_W, arg.type->getSizeWords()},
+            IR::Arg_Nul(), IR::Arg_Stk());
+      }
+
+      //
+      // GenStmnt_SetBitsPartT<IR::Arg_Stk>
+      //
+      template<> void GenStmnt_SetBitsPartT<IR::Arg_Stk>(SR::Exp const *exp,
+         SR::GenStmntCtx const &, SR::Arg const &, Core::FastU, Core::FastU)
+      {
+         throw Core::ExceptStr(exp->pos, "AddrBase::Stk set");
+      }
+   }
+}
 
 
 //----------------------------------------------------------------------------|
@@ -52,23 +164,23 @@ namespace GDCC
 
             if(dst.type->getQualAddr().base == IR::AddrBase::Nul)
             {
-               // TODO: Do not require an IR Arg.
-               if(!expL->getArg().isIRArg())
-                  throw Core::ExceptStr(pos, "bitfield write must be IR arg");
-               IR::Arg argL = expL->getArg().getIRArg(ctx.prog);
+               // Evaluate source to stack.
+               expR->genStmntStk(ctx);
 
-               // Generate IR arg for right operand.
-               IR::Arg argR;
-               if(!expR->getArg().isIRArg())
+               auto argL = expL->getArg();
+
+               // Map from generic address space for codegen.
+               if(argL.type->getQualAddr().base == IR::AddrBase::Gen)
+                  argL.type = argL.type->getTypeQual(IR::GetAddrGen());
+
+               switch(argL.type->getQualAddr().base)
                {
-                  expR->genStmntStk(ctx);
-                  argR = IR::Arg_Stk();
+                  #define GDCC_IR_AddrList(addr) \
+                  case IR::AddrBase::addr: \
+                     GenStmnt_SetBitsPartT<IR::Arg_##addr>(this, ctx, argL, bits, offs); \
+                     break;
+                  #include "IR/AddrList.hpp"
                }
-               else
-                  argR = expR->getArg().getIRArg(ctx.prog);
-
-               ctx.block.addStatementArgs({IR::Code::Bset_W, typeL->getSizeWords()},
-                  argL, argR, bits, offs);
             }
             else
                throw Core::ExceptStr(pos, "bitfield write-and-read unimplemented");
