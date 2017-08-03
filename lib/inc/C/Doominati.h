4f775f48ad23aeb4a1a25725900df80d90510cbc
David Hill <DavidPH55@verizon.net>
2017-08-02 20:54:24 -0400

    libdge: More Doominati.h cleanup

--- lib/inc/C/Doominati.h
+++ lib/inc/C/Doominati.h
@@ -86,10 +86,10 @@
 //
 #define DGE_Object_MemberGet(T, ...) \
    _Generic((T)0, \
-        signed       int:    DGE_ObjectMemberGetI, \
-      unsigned long  _Fract: DGE_ObjectMemberGetLA, \
-      unsigned       int:    DGE_ObjectMemberGetU, \
-        signed short _Accum: DGE_ObjectMemberGetX \
+      int:         DGE_ObjectMemberGetI, \
+      DGE_ULFract: DGE_ObjectMemberGetLA, \
+      unsigned:    DGE_ObjectMemberGetU, \
+        DGE_Accum: DGE_ObjectMemberGetX \
    )(__VA_ARGS__)
 
 //
@@ -97,10 +97,10 @@
 //
 #define DGE_Object_MemberSet(T, ...) \
    _Generic((T)0, \
-        signed       int:    DGE_ObjectMemberSetI, \
-      unsigned long  _Fract: DGE_ObjectMemberSetLA, \
-      unsigned       int:    DGE_ObjectMemberSetU, \
-        signed short _Accum: DGE_ObjectMemberSetX \
+      int:         DGE_ObjectMemberSetI, \
+      DGE_ULFract: DGE_ObjectMemberSetLA, \
+      unsigned:    DGE_ObjectMemberSetU, \
+      DGE_Accum:   DGE_ObjectMemberSetX \
    )(__VA_ARGS__)
 
 //
@@ -128,13 +128,46 @@
 // Types                                                                      |
 //
 
+//
+// DGE_Accum
+//
+#if __GDCC__
+typedef short _Accum DGE_Accum;
+#else
+typedef int DGE_Accum;
+#endif
+
+//
+// DGE_CallbackType
+//
+typedef void (*DGE_CallbackType)(void) DGE_Callback;
+
+//
+// DGE_LFract
+//
+#if __GDCC__
+typedef long _Fract DGE_LFract;
+#else
+typedef int DGE_LFract;
+#endif
+
+//
+// DGE_Str
+//
 #if __GDCC__
 typedef __str DGE_Str;
 #else
 typedef unsigned DGE_Str;
 #endif
 
-typedef void (*DGE_CallbackType)(void) DGE_Callback;
+//
+// DGE_ULFract
+//
+#if __GDCC__
+typedef unsigned long _Fract DGE_ULFract;
+#else
+typedef unsigned DGE_ULFract;
+#endif
 
 enum // DGE_Align
 {
@@ -171,7 +204,7 @@ enum // DGE_Button
 
 typedef struct DGE_Point2
 {
-   short _Accum x, y;
+   DGE_Accum x, y;
 } DGE_Point2;
 
 typedef struct DGE_Point2I
@@ -193,12 +226,13 @@ struct DGE_Object;
 // Extern Functions                                                           |
 //
 
-DGE_Native unsigned DGE_BlockMap_Find(short _Accum xl, short _Accum yl, short _Accum xh, short _Accum yh);
+DGE_Native unsigned DGE_BlockMap_Find(DGE_Accum xl, DGE_Accum yl, DGE_Accum xh, DGE_Accum yh);
 DGE_Native unsigned DGE_BlockMap_FindCountSector(unsigned find);
 DGE_Native unsigned DGE_BlockMap_FindCountThinker(unsigned find);
 DGE_Native void DGE_BlockMap_FindFree(unsigned find);
 DGE_Native unsigned DGE_BlockMap_FindGetSector(unsigned find, unsigned idx);
 DGE_Native unsigned DGE_BlockMap_FindGetThinker(unsigned find, unsigned idx);
+DGE_Native void DGE_BlockMap_Split(DGE_Accum minSize, unsigned maxObj);
 
 DGE_Native void DGE_CallbackRegister(char const *name, DGE_CallbackType callback);
 DGE_Native void DGE_CallbackUnregister(char const *name, DGE_CallbackType callback);
@@ -218,26 +252,25 @@ DGE_Native void DGE_DebugLocReg(void);
 
 DGE_Native void DGE_Delay(unsigned ticks);
 
-DGE_Native void DGE_DopplerSpeed(short _Accum meterspersecond);
+DGE_Native void DGE_DopplerSpeed(DGE_Accum meterspersecond);
 
 DGE_OptArgs(1)
-DGE_Native void DGE_DrawColor(unsigned long _Fract r,
-   unsigned long _Fract g, unsigned long _Fract b, unsigned long _Fract a);
-DGE_Native void DGE_DrawCircle(short _Accum x, short _Accum y, short _Accum r);
-DGE_Native void DGE_DrawCircleLine(short _Accum x, short _Accum y, short _Accum r);
+DGE_Native void DGE_DrawColor(DGE_ULFract r, DGE_ULFract g, DGE_ULFract b, DGE_ULFract a);
+DGE_Native void DGE_DrawCircle(DGE_Accum x, DGE_Accum y, DGE_Accum r);
+DGE_Native void DGE_DrawCircleLine(DGE_Accum x, DGE_Accum y, DGE_Accum r);
 DGE_Native void DGE_DrawCirclePrecision(int subdivisions);
-DGE_Native void DGE_DrawEllipse(short _Accum x1, short _Accum y1, short _Accum x2, short _Accum y2);
-DGE_Native void DGE_DrawEllipseLine(short _Accum x1, short _Accum y1, short _Accum x2, short _Accum y2);
-DGE_Native void DGE_DrawRectangle(short _Accum x1, short _Accum y1, short _Accum x2, short _Accum y2);
-DGE_Native void DGE_DrawRectangleLine(short _Accum x1, short _Accum y1, short _Accum x2, short _Accum y2);
+DGE_Native void DGE_DrawEllipse(DGE_Accum x1, DGE_Accum y1, DGE_Accum x2, DGE_Accum y2);
+DGE_Native void DGE_DrawEllipseLine(DGE_Accum x1, DGE_Accum y1, DGE_Accum x2, DGE_Accum y2);
+DGE_Native void DGE_DrawRectangle(DGE_Accum x1, DGE_Accum y1, DGE_Accum x2, DGE_Accum y2);
+DGE_Native void DGE_DrawRectangleLine(DGE_Accum x1, DGE_Accum y1, DGE_Accum x2, DGE_Accum y2);
 DGE_Native void DGE_DrawShader(unsigned shd);
-DGE_Native void DGE_DrawLine(short _Accum x1, short _Accum y1, short _Accum x2, short _Accum y2);
-DGE_Native void DGE_DrawLineSmooth(_Bool on);
+DGE_Native void DGE_DrawLine(DGE_Accum x1, DGE_Accum y1, DGE_Accum x2, DGE_Accum y2);
+DGE_Native void DGE_DrawLineSmooth(unsigned on);
 DGE_Native void DGE_DrawLineWidth(int width);
-DGE_Native void DGE_DrawText(short _Accum x, short _Accum y, char const *str);
+DGE_Native void DGE_DrawText(DGE_Accum x, DGE_Accum y, char const *str);
 DGE_Native void DGE_DrawTexture(unsigned tex);
-DGE_Native void DGE_DrawTriangle(short _Accum x1, short _Accum y1, short _Accum x2, short _Accum y2, short _Accum x3, short _Accum y3);
-DGE_Native void DGE_DrawTriangleLine(short _Accum x1, short _Accum y1, short _Accum x2, short _Accum y2, short _Accum x3, short _Accum y3);
+DGE_Native void DGE_DrawTriangle(DGE_Accum x1, DGE_Accum y1, DGE_Accum x2, DGE_Accum y2, DGE_Accum x3, DGE_Accum y3);
+DGE_Native void DGE_DrawTriangleLine(DGE_Accum x1, DGE_Accum y1, DGE_Accum x2, DGE_Accum y2, DGE_Accum x3, DGE_Accum y3);
 
 DGE_Native void DGE_FileClose(int fd);
 DGE_Native int DGE_FileGet(int fd, unsigned idx);
@@ -248,7 +281,7 @@ DGE_Native unsigned DGE_FileSize(int fd);
 DGE_Native void *DGE_FreestoreBegin(void);
 DGE_Native void *DGE_FreestoreEnd(void);
 
-DGE_Native long _Fract DGE_GetInputAxis(unsigned num, unsigned axis);
+DGE_Native DGE_LFract DGE_GetInputAxis(unsigned num, unsigned axis);
 DGE_Native unsigned DGE_GetInputButtons(void);
 DGE_Native DGE_Point2I DGE_GetInputCursor(void);
 DGE_Native unsigned DGE_GetSound(DGE_Str name);
@@ -256,13 +289,13 @@ DGE_Native unsigned DGE_GetShader(DGE_Str name);
 DGE_Native unsigned DGE_GetTexture(DGE_Str name);
 
 DGE_Native int DGE_ObjectMemberGetI(unsigned id, unsigned mem);
-DGE_Native unsigned long _Fract DGE_ObjectMemberGetLA(unsigned id, unsigned mem);
+DGE_Native DGE_ULFract DGE_ObjectMemberGetLA(unsigned id, unsigned mem);
 DGE_Native unsigned DGE_ObjectMemberGetU(unsigned id, unsigned mem);
-DGE_Native short _Accum DGE_ObjectMemberGetX(unsigned id, unsigned mem);
+DGE_Native DGE_Accum DGE_ObjectMemberGetX(unsigned id, unsigned mem);
 DGE_Native void DGE_ObjectMemberSetI(unsigned id, unsigned mem, int val);
-DGE_Native void DGE_ObjectMemberSetLA(unsigned id, unsigned mem, unsigned long _Fract val);
+DGE_Native void DGE_ObjectMemberSetLA(unsigned id, unsigned mem, DGE_ULFract val);
 DGE_Native void DGE_ObjectMemberSetU(unsigned id, unsigned mem, unsigned val);
-DGE_Native void DGE_ObjectMemberSetX(unsigned id, unsigned mem, short _Accum val);
+DGE_Native void DGE_ObjectMemberSetX(unsigned id, unsigned mem, DGE_Accum val);
 DGE_Native void DGE_ObjectRefAdd(unsigned id);
 DGE_Native void DGE_ObjectRefSub(unsigned id);
 
@@ -282,25 +315,25 @@ DGE_Native void DGE_SetViewpoint(unsigned id);
 DGE_Native void DGE_SetVirtualResolution(unsigned w, unsigned h);
 DGE_Native void DGE_SetWindowTitle(char const *str);
 
-DGE_Native void DGE_SoundChanLoop(unsigned src, unsigned chan, _Bool on);
+DGE_Native void DGE_SoundChanLoop(unsigned src, unsigned chan, unsigned on);
 DGE_OptArgs(1)
-DGE_Native void DGE_SoundChanPitch(unsigned src, unsigned chan, unsigned long _Fract pitch);
+DGE_Native void DGE_SoundChanPitch(unsigned src, unsigned chan, DGE_ULFract pitch);
 DGE_Native void DGE_SoundChanPlay(unsigned src, unsigned chan);
-DGE_Native void DGE_SoundChanPosition(unsigned src, unsigned chan, short _Accum x, short _Accum y, short _Accum z);
+DGE_Native void DGE_SoundChanPosition(unsigned src, unsigned chan, DGE_Accum x, DGE_Accum y, DGE_Accum z);
 DGE_Native void DGE_SoundChanStop(unsigned src, unsigned chan);
-DGE_Native void DGE_SoundChanVolume(unsigned src, unsigned chan, unsigned long _Fract volume);
+DGE_Native void DGE_SoundChanVolume(unsigned src, unsigned chan, DGE_ULFract volume);
 
 DGE_OptArgs(4)
-DGE_Native void DGE_SoundListener(short _Accum x, short _Accum y, short _Accum z, short _Accum velx, short _Accum vely, short _Accum velz, short _Accum angle);
+DGE_Native void DGE_SoundListener(DGE_Accum x, DGE_Accum y, DGE_Accum z, DGE_Accum velx, DGE_Accum vely, DGE_Accum velz, DGE_Accum angle);
 
-DGE_Native unsigned DGE_SoundSource(short _Accum x, short _Accum y, short _Accum z);
+DGE_Native unsigned DGE_SoundSource(DGE_Accum x, DGE_Accum y, DGE_Accum z);
 DGE_OptArgs(1)
 DGE_Native unsigned DGE_SoundSrcBind(unsigned src, unsigned snd, unsigned chan);
 DGE_Native void DGE_SoundSrcDestroy(unsigned src);
 DGE_OptArgs(1)
 DGE_Native unsigned DGE_SoundSrcPlay(unsigned src, unsigned snd, unsigned chan);
-DGE_Native void DGE_SoundSrcPosition(unsigned src, short _Accum x, short _Accum y, short _Accum z);
-DGE_Native void DGE_SoundSrcVelocity(unsigned src, short _Accum x, short _Accum y, short _Accum z);
+DGE_Native void DGE_SoundSrcPosition(unsigned src, DGE_Accum x, DGE_Accum y, DGE_Accum z);
+DGE_Native void DGE_SoundSrcVelocity(unsigned src, DGE_Accum x, DGE_Accum y, DGE_Accum z);
 
 DGE_Native unsigned DGE_SysRead(char *buf, unsigned len);
 DGE_Native unsigned DGE_SysWrite(char const *buf, unsigned len);
@@ -334,15 +367,15 @@ typedef struct DGE_Object
 // DGE_Sector
 //
 #define DGE_SectorProps() DGE_ObjectProps() \
-   DGE_PropMem(unsigned       int,    pc) \
-   DGE_PropMem(unsigned       int,    texc) \
-   DGE_PropMem(unsigned       int,    texf) \
-   DGE_PropMem(  signed short _Accum, xh) \
-   DGE_PropMem(  signed short _Accum, xl) \
-   DGE_PropMem(  signed short _Accum, yh) \
-   DGE_PropMem(  signed short _Accum, yl) \
-   DGE_PropMem(  signed short _Accum, zh) \
-   DGE_PropMem(  signed short _Accum, zl)
+   DGE_PropMem(unsigned,  pc) \
+   DGE_PropMem(unsigned,  texc) \
+   DGE_PropMem(unsigned,  texf) \
+   DGE_PropMem(DGE_Accum, xh) \
+   DGE_PropMem(DGE_Accum, xl) \
+   DGE_PropMem(DGE_Accum, yh) \
+   DGE_PropMem(DGE_Accum, yl) \
+   DGE_PropMem(DGE_Accum, zh) \
+   DGE_PropMem(DGE_Accum, zl)
 typedef struct DGE_Sector
 {
    int id;
@@ -354,8 +387,8 @@ typedef struct DGE_Sector
 // DGE_Thinker
 //
 #define DGE_ThinkerProps() DGE_ObjectProps() \
-   DGE_PropMem(unsigned int, next) \
-   DGE_PropMem(unsigned int, prev)
+   DGE_PropMem(unsigned, next) \
+   DGE_PropMem(unsigned, prev)
 typedef struct DGE_Thinker
 {
    int id;
@@ -367,11 +400,11 @@ typedef struct DGE_Thinker
 // DGE_PointThinker
 //
 #define DGE_PointThinkerProps() DGE_ThinkerProps() \
-   DGE_PropMem(unsigned long  _Fract, pitch) \
-   DGE_PropMem(  signed short _Accum, x) \
-   DGE_PropMem(  signed short _Accum, y) \
-   DGE_PropMem(unsigned long  _Fract, yaw) \
-   DGE_PropMem(  signed short _Accum, z)
+   DGE_PropMem(DGE_ULFract, pitch) \
+   DGE_PropMem(DGE_Accum,   x) \
+   DGE_PropMem(DGE_Accum,   y) \
+   DGE_PropMem(DGE_ULFract, yaw) \
+   DGE_PropMem(DGE_Accum,   z)
 typedef struct DGE_PointThinker
 {
    int id;
@@ -383,9 +416,9 @@ typedef struct DGE_PointThinker
 // DGE_RenderThinker
 //
 #define DGE_RenderThinkerProps() DGE_PointThinkerProps() \
-   DGE_PropMem(  signed short _Accum, rsx) \
-   DGE_PropMem(  signed short _Accum, rsy) \
-   DGE_PropMem(unsigned       int,    sprite)
+   DGE_PropMem(DGE_Accum, rsx) \
+   DGE_PropMem(DGE_Accum, rsy) \
+   DGE_PropMem(unsigned,  sprite)
 typedef struct DGE_RenderThinker
 {
    int id;
@@ -397,13 +430,13 @@ typedef struct DGE_RenderThinker
 // DGE_PhysicsThinker
 //
 #define DGE_PhysicsThinkerProps() DGE_RenderThinkerProps() \
-   DGE_PropMem(unsigned       int,    mass) \
-   DGE_PropMem(  signed short _Accum, sx) \
-   DGE_PropMem(  signed short _Accum, sy) \
-   DGE_PropMem(  signed short _Accum, sz) \
-   DGE_PropMem(  signed short _Accum, vx) \
-   DGE_PropMem(  signed short _Accum, vy) \
-   DGE_PropMem(  signed short _Accum, vz)
+   DGE_PropMem(unsigned,  mass) \
+   DGE_PropMem(DGE_Accum, sx) \
+   DGE_PropMem(DGE_Accum, sy) \
+   DGE_PropMem(DGE_Accum, sz) \
+   DGE_PropMem(DGE_Accum, vx) \
+   DGE_PropMem(DGE_Accum, vy) \
+   DGE_PropMem(DGE_Accum, vz)
 typedef struct DGE_PhysicsThinker
 {
    int id;
@@ -415,7 +448,7 @@ typedef struct DGE_PhysicsThinker
 // DGE_Entity
 //
 #define DGE_EntityProps() DGE_PhysicsThinkerProps() \
-   DGE_PropMem(signed int, health)
+   DGE_PropMem(int, health)
 typedef struct DGE_Entity
 {
    int id;
@@ -427,8 +460,8 @@ typedef struct DGE_Entity
 // DGE_MissileEntity
 //
 #define DGE_MissileEntityProps() DGE_EntityProps() \
-   DGE_PropMem(unsigned int, damage) \
-   DGE_PropMem(unsigned int, owner)
+   DGE_PropMem(unsigned, damage) \
+   DGE_PropMem(unsigned, owner)
 typedef struct DGE_MissileEntity
 {
    int id;
