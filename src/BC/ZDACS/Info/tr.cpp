7637d2dfdabc1034df8d5d6dcd2e388ed838e654
David Hill <DavidPH55@verizon.net>
2018-06-22 23:50:48 -0400

    BC::ZDACS: Add check for unknown script types

--- src/BC/ZDACS/Info/tr.cpp
+++ src/BC/ZDACS/Info/tr.cpp
@@ -1,6 +1,6 @@
 //-----------------------------------------------------------------------------
 //
-// Copyright (C) 2013-2017 David Hill
+// Copyright (C) 2013-2018 David Hill
 //
 // See COPYING for license information.
 //
@@ -11,6 +11,8 @@
 //-----------------------------------------------------------------------------
 
 #include "BC/ZDACS/Info.hpp"
+
+#include "Core/Exception.hpp"
 
 #include "IR/CallType.hpp"
 #include "IR/Function.hpp"
@@ -35,6 +37,37 @@ namespace GDCC::BC::ZDACS
       case IR::CallType::ScriptS:
          if(!func->valueStr)
             func->valueStr = func->glyph;
+
+         // Check script types.
+         for(auto const &st : func->stype) switch(st)
+         {
+         // Flags.
+         case Core::STR_Clientside:
+         case Core::STR_Net:
+
+         // Types.
+         case Core::STR_BlueReturn:
+         case Core::STR_Death:
+         case Core::STR_Disconnect:
+         case Core::STR_Enter:
+         case Core::STR_Event:
+         case Core::STR_Kill:
+         case Core::STR_Lightning:
+         case Core::STR_Open:
+         case Core::STR_Pickup:
+         case Core::STR_RedReturn:
+         case Core::STR_Reopen:
+         case Core::STR_Respawn:
+         case Core::STR_Return:
+         case Core::STR_Unloading:
+         case Core::STR_WhiteReturn:
+            break;
+
+         default:
+            throw Core::ParseExceptExpect(func->getOrigin(),
+               "ZDACS script type", st, false);
+         }
+
          break;
 
       default: break;
