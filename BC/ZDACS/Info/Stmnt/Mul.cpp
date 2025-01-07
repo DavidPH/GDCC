6cde100579bc7e3552c242b632202e30036a80b1
David Hill <DavidPH55@verizon.net>
2025-01-07 10:05:56 -0500

    BC::ZDACS: Add fixed-mul based expanding multiply
    This new version is about 42 ops faster. I considered an alternate
    version with a two-deep case-table-jump on the high-low-rounding bits,
    which would have improved the non-worst-case performance, but with 16
    unique cases (out of 64 combinations of bits) I decided it was not
    worth the amount of codegen.

--- BC/ZDACS/Info/Stmnt/Mul.cpp
+++ BC/ZDACS/Info/Stmnt/Mul.cpp
@@ -1,6 +1,6 @@
 //-----------------------------------------------------------------------------
 //
-// Copyright (C) 2015-2019 David Hill
+// Copyright (C) 2015-2025 David Hill
 //
 // See COPYING for license information.
 //
@@ -38,39 +38,60 @@ namespace GDCC::BC::ZDACS
       GDCC_BC_AddFuncPre((Code::MulX+U, 1), 2, 2, 5, __FILE__);
       GDCC_BC_AddFuncObjBin(1, 1);
 
-      GDCC_BC_AddFuncObjReg(llo, 1, 0);
-      GDCC_BC_AddFuncObjReg(lhi, 1, 1);
-      GDCC_BC_AddFuncObjReg(rlo, 1, 2);
-      GDCC_BC_AddFuncObjReg(rhi, 1, 3);
-      GDCC_BC_AddFuncObjReg(tmp, 1, 4);
+      IR::Glyph labelCheckRop{prog, name + "$checkRop"};
+      IR::Glyph labelShiftLow{prog, name + "$shiftLow"};
 
-      // Split arguments to 16-bit pieces. Take care due to aliasing.
-      GDCC_BC_AddStmnt(Code::BAnd,  1, rlo, rop, 0xFFFF);
-      GDCC_BC_AddStmnt(Code::ShR+U, 1, rhi, rop, 16);
-      GDCC_BC_AddStmnt(Code::ShR+U, 1, lhi, lop, 16);
-      GDCC_BC_AddStmnt(Code::BAnd,  1, llo, lop, 0xFFFF);
+      // Calculate low word using normal multiply.
+      GDCC_BC_AddStmnt(Code::Mul+U, 1, stk, lop, rop);
 
-      // Column 0.
-      GDCC_BC_AddStmnt(Code::Mul+U, 1, stk, llo, rlo);
-      GDCC_BC_AddStmnt(Code::Move,  1, stk, 0);
+      // Calculate bottom 16 bits of high word using a fixed-mul.
+      // Fixed-mul essentially gives up bits 16-47 of an expanding multiply, so
+      // taking the top 16 bits would give us the 16 bits we want.
+      // However, because it is a signed operation, if the high bit is set in
+      // an operand, it gets sign-extended, ruining the result.
+      // This can be overcome by simply discarding the low bit, leaving the
+      // high bit always unset.
+      // However, if the discarded bit was set, this sacrifices one bit of the
+      // output we need for each operand.
+      // This can be overcome by adding in the high bits of the opposite
+      // operand into the fixed-mul result, so long as it is rounded half-up.
+      GDCC_BC_AddStmnt(Code::ShR+U, 1, stk, lop, 1);
+      GDCC_BC_AddStmnt(Code::ShR+U, 1, stk, rop, 1);
+      GDCC_BC_AddStmnt(Code::Mul+X, 1, stk, stk, stk);
+      GDCC_BC_AddStmnt(Code::ShL+U, 1, stk, stk, 2);
 
-      // Column 1.
-      GDCC_BC_AddStmnt(Code::Mul+U, 1, tmp, llo, rhi);
-      GDCC_BC_AddStmnt(Code::ShL+U, 1, stk, tmp, 16);
-      GDCC_BC_AddStmnt(Code::ShR+U, 1, stk, tmp, 16);
+      GDCC_BC_AddStmnt(Code::BAnd,     1, stk, lop, 0x0001);
+      GDCC_BC_AddStmnt(Code::Jcnd_Nil, 1, stk, labelCheckRop);
+      GDCC_BC_AddStmnt(Code::ShR+U,    1, stk, rop, 16);
+      GDCC_BC_AddStmnt(Code::Add+U,    1, stk, stk, stk);
+      GDCC_BC_AddStmnt(Code::BAnd,     1, stk, rop, 0x8000);
+      GDCC_BC_AddStmnt(Code::Jcnd_Nil, 1, stk, labelCheckRop);
+      GDCC_BC_AddStmnt(Code::Add+U,    1, stk, stk, 1);
 
-      GDCC_BC_AddStmnt(Code::Mul+U, 1, tmp, lhi, rlo);
-      GDCC_BC_AddStmnt(Code::ShL+U, 1, stk, tmp, 16);
-      GDCC_BC_AddStmnt(Code::ShR+U, 1, stk, tmp, 16);
+      GDCC_BC_AddLabel(labelCheckRop);
+      GDCC_BC_AddStmnt(Code::BAnd,     1, stk, rop, 0x0001);
+      GDCC_BC_AddStmnt(Code::Jcnd_Nil, 1, stk, labelShiftLow);
+      GDCC_BC_AddStmnt(Code::ShR+U,    1, stk, lop, 16);
+      GDCC_BC_AddStmnt(Code::Add+U,    1, stk, stk, stk);
+      GDCC_BC_AddStmnt(Code::BAnd,     1, stk, lop, 0x8000);
+      GDCC_BC_AddStmnt(Code::Jcnd_Nil, 1, stk, labelShiftLow);
+      GDCC_BC_AddStmnt(Code::Add+U,    1, stk, stk, 1);
 
-      // Column 2.
-      GDCC_BC_AddStmnt(Code::Mul+U, 1, stk, lhi, rhi);
-      GDCC_BC_AddStmnt(Code::Add+U, 1, stk, stk, stk);
+      GDCC_BC_AddLabel(labelShiftLow);
+      GDCC_BC_AddStmnt(Code::ShR+U, 1, stk, stk, 16);
 
-      GDCC_BC_AddStmnt(Code::Add+U, 2, stk, stk, stk);
-      GDCC_BC_AddStmnt(Code::Add+U, 2, stk, stk, stk);
+      // Calculate top 16 bits of high word using a shifted fixed-mul.
+      // Shifting the two arguments by 8 shifts the result window over 16 bits,
+      // but sacrifices the lower 16 bits of precision.
+      GDCC_BC_AddStmnt(Code::ShR+U, 1, stk, lop, 8);
+      GDCC_BC_AddStmnt(Code::ShR+U, 1, stk, rop, 8);
+      GDCC_BC_AddStmnt(Code::Mul+X, 1, stk, stk, stk);
+      GDCC_BC_AddStmnt(Code::BAnd,  1, stk, stk, 0xFFFF0000);
 
-      GDCC_BC_AddStmnt(Code::Retn,  2, stk);
+      // Combine both halves of high word.
+      GDCC_BC_AddStmnt(Code::BOrI, 1, stk, stk, stk);
+
+      GDCC_BC_AddStmnt(Code::Retn, 2, stk);
 
       GDCC_BC_AddFuncEnd();
    }
